f8995c351b827e19ea8f470940de91f902e8b32f
Rakesh Rathi <rakesh.open.source@gmail.com>
2020-04-10 01:25:58 +0530

    cleaning

--- pusher-presence-private-channel-demo/src/main/java/pusherserver/newpusherserver/Constants.java
+++ pusher-presence-private-channel-demo/src/main/java/pusherserver/newpusherserver/Constants.java
@@ -1,9 +1,9 @@
 package pusherserver.newpusherserver;
 
 public class Constants {
-    public static final String APPID = "979248";
+    public static final String APPID = "<app-id>";
     public static final String KEY = "bc1c84f6ee58bf65ee12";
-    public static final String SECRET = "367648d0bd72697e9859";
+    public static final String SECRET = "<secret_key>";
     public static final String PRESENCE_CHANNEL = "presence-test";
     public static final String PRIVATE_CHANNEL = "private-test";
     public static final String EVENT_NAME= "testevent";
